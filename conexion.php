8899d47eb4d330a40482e31e3f55ae1b6bf876e0
Facundo <facuunion13@gmail.com>
2021-07-12 21:17:00 -0300

    Docker anda  en docker de debian es sensible las letras

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php 
     if (isset($_SESSION['usuario'])){
-        $base= 'bd_'. $_SESSION['usuario'];
+        $base= 'BD_'. $_SESSION['usuario'];
         $db_user = new mysqli('localhost', $_SESSION['usuario'], $_SESSION['pass'], $base, 3306); //Base del usuario
         $link = new mysqli('localhost','root','','prueba'); //Base principal del sistema
     }
